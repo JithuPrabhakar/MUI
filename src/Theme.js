71bcc3f6a0c23c0c2ced8eb9cce20dc369f160cd
Jithu Prabhakar <join.jitz@gmail.com>
2023-03-28 17:43:53 +0530

    frame complete

---
+++ src/Theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary:{
+            main: "#0052cc",
+            light: "lightblue",
+        },
+        secondary: {
+          main: '#edf2ff',
+          light: "lightorange",
+        },
+    }
+})
